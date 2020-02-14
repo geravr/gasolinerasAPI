895384bfa8fb8118223d69bf40cc4b75e4040fe4
geravr <gera.villa21@gmail.com>
2020-02-13 23:48:34 -0600

    add pin on map

--- js/UI.js
+++ js/UI.js
@@ -1,21 +1,52 @@
 class UI {
-    constructor() {
+  constructor() {
+    this.api = new API();
 
-         // Iniciar el mapa
-         this.mapa = this.initializeMap();
+    // create LayerGroup markers
+    this.markers = new L.LayerGroup();
 
-    }
+    this.map = this.initializeMap();
+  }
 
-    initializeMap() {
-         // Inicializar y obtener la propiedad del mapa
-         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
-         const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-         L.tileLayer(
-             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-             attribution: '&copy; ' + mapLink + ' Contributors',
-             maxZoom: 18,
-             }).addTo(map);
-         return map;
+  initializeMap() {
+    // Initialize & get map property
+    const map = L.map("mapa").setView([20.6506194, -103.3214156], 11.5);
+    const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; " + mapLink + " Contributors",
+      maxZoom: 18
+    }).addTo(map);
+    return map;
+  }
 
-    }
+  showGasStation() {
+    this.api.getData().then(data => {
+      const response = data.responseJSON.results;
+
+      // ejecutar
+      this.showPins(response);
+    });
+  }
+
+  showPins(data) {
+      // Clean markers
+    this.markers.clearLayers();
+
+    // Iterate de gas station
+    data.forEach(data => {
+        const {latitude, longitude, calle, regular, premium} = data
+        let floatLatitude = parseFloat(latitude);
+        let floatLongitude = parseFloat(longitude);
+
+        if (floatLatitude >= 20.512000 && floatLatitude <= 20.815000 && floatLongitude <= -103.166000 && floatLongitude >= -103.480000 ) {
+                 // add PIN
+        const marker =  new L.marker([
+            parseFloat(latitude),
+            parseFloat(longitude)
+        ]);
+        this.markers.addLayer(marker);
+        }
+    });
+    this.markers.addTo(this.map);
+  }
 }
